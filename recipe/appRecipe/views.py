d669182059dd221f86bc8f3a4e04e7212e3039ef
Erik Sanders <sanderej@rose-hulman.edu>
2013-04-02 12:30:00 -0400

    added recipe index template that should list out recipes in the DB

--- recipe/appRecipe/views.py
+++ recipe/appRecipe/views.py
@@ -1,7 +1,15 @@
 from django.http import HttpResponse
+from django.template import Context, loader
+
+from appRecipe.models import Recipe
 
 def recipeIndex(request):
-  return HttpResponse("Hello, world. You're at the recipe index.")
+  recipe_list = Recipe.objects.all()
+  template = loader.get_template('recipe/recipeIndex.html')
+  context = Context({
+    'recipe_list': recipe_list,
+  })
+  return HttpResponse(template.render(context))
   
 def recipeDetail(request, recipe_id):
   return HttpResponse("You're looking at recipe %s." % recipe_id)
