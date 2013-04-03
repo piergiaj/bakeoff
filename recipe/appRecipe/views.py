2ca5b61ed26acae3d8c13c543e26dcef1789ea2e
Erik Sanders <sanderej@rose-hulman.edu>
2013-04-03 00:03:15 -0400

    added a simple 404 and 500 template that aren't used until we set Debug = false in settings. Also added the recipe detail view to actually pull data from the db.

--- recipe/appRecipe/views.py
+++ recipe/appRecipe/views.py
@@ -1,18 +1,18 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.template import Context, loader
+from django.shortcuts import render, get_object_or_404 #, get_list_or_404
 
 from appRecipe.models import Recipe
 
 def recipeIndex(request):
   recipe_list = Recipe.objects.all()
   template = loader.get_template('recipe/recipeIndex.html')
-  context = Context({
-    'recipe_list': recipe_list,
-  })
-  return HttpResponse(template.render(context))
+  context = { 'recipe_list': recipe_list,}
+  return render(request, 'recipe/recipeIndex.html',context)
   
 def recipeDetail(request, recipe_id):
-  return HttpResponse("You're looking at recipe %s." % recipe_id)
+  recipe = get_object_or_404(Recipe, pk=recipe_id)
+  return render(request, 'recipe/recipeDetail.html', {'recipe':recipe})
   
 def recipeIngredients(request, recipe_id):
   return HttpResponse("You're looking at the ingredients for recipe %s." % recipe_id)
