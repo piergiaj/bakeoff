5e94dbbf0ba24a4ca6592b85eb483e5d6a440053
Erik Sanders <sanderej@rose-hulman.edu>
2013-04-03 11:41:50 -0400

    added some bootstrap components and some extra test pages

--- recipe/appRecipe/urls.py
+++ recipe/appRecipe/urls.py
@@ -3,10 +3,14 @@ from django.conf.urls import patterns, url
 from appRecipe import views
 
 urlpatterns = patterns('',
+  url(r'^$', views.home, name='home'),
   # ex: /recipes/
   url(r'^recipes/$', views.recipeIndex, name='recipeIndex'),
   # ex: /recipes/3/
   url(r'^recipes/(?P<recipe_id>\d+)/$', views.recipeDetail, name='recipeDetail'),
   # ex: /recipes/3/ingredients
   url(r'^recipes/(?P<recipe_id>\d+)/ingredients/$', views.recipeIngredients, name='recipeIngredients'),
+
+  url(r'^chefs/$', views.chefIndex, name='chefIndex'),
+  url(r'^chefs/(?P<chef_id>\d+)/$', views.chefDetail, name='chefDetail'),
 )
--- recipe/appRecipe/views.py
+++ recipe/appRecipe/views.py
@@ -2,11 +2,17 @@ from django.http import HttpResponse, Http404
 from django.template import Context, loader
 from django.shortcuts import render, get_object_or_404 #, get_list_or_404
 
-from appRecipe.models import Recipe
+from appRecipe.models import Recipe, Chef
+
+def home(request):
+  recipe_list = Recipe.objects.all()
+  chef_list = Chef.objects.all()
+  context = { 'chef_list' : chef_list,
+              'recipe_list': recipe_list, }
+  return render(request, 'recipe/home.html', context)
 
 def recipeIndex(request):
   recipe_list = Recipe.objects.all()
-  template = loader.get_template('recipe/recipeIndex.html')
   context = { 'recipe_list': recipe_list,}
   return render(request, 'recipe/recipeIndex.html',context)
   
@@ -16,3 +22,12 @@ def recipeDetail(request, recipe_id):
   
 def recipeIngredients(request, recipe_id):
   return HttpResponse("You're looking at the ingredients for recipe %s." % recipe_id)
+
+def chefIndex(request):
+  chef_list = Chef.objects.all()
+  context = { 'chef_list' : chef_list }
+  return render(request, 'recipe/chefIndex.html', context)
+
+def chefDetail(request, chef_id):
+  chef = get_object_or_404(Chef, pk=chef_id)
+  return render(request, 'recipe/chefDetail.html', {'chef':chef})
