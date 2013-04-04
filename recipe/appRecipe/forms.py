cfe6cc5f05af005057617430ae5172932376baa3
AJ Piergiovanni <piergiaj@ubuntu.(none)>
2013-04-04 15:08:51 -0400

    adding forms

---
+++ recipe/appRecipe/forms.py
@@ -0,0 +1,16 @@
+from django import forms
+
+class AddChef(forms.Form):
+	name = forms.CharField(max_length=50, required=True)
+	email = forms.EmailField(required=True)
+	password = forms.CharField(required=True, widget=forms.PasswordInput)
+
+
+class AddRecipe(forms.Form):
+	recipe_Name = forms.CharField()
+	prep_Time = forms.IntegerField()
+	cook_Time = forms.IntegerField()
+	comments = forms.CharField()
+	picture = forms.ImageField()
+
+
