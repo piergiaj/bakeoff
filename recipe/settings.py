198cb2590bdb765ecad766a59e070afa7b4fdcc3
Erik Sanders <sanderej@rose-hulman.edu>
2013-04-17 02:31:43 -0400

    fixed maybs

--- recipe/settings.py
+++ recipe/settings.py
@@ -1,9 +1,7 @@
 # Django settings for recipe project.
 import os.path
 
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
-
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -65,7 +63,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_PATH,'appRecipe/static')
+STATIC_ROOT = os.path.abspath('appRecipe/static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -79,8 +77,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_PATH,'appRecipe/static'),
-    os.path.join(PROJECT_PATH,'static'),
+    os.path.abspath('appRecipe/static'),
 )
 
 # List of finder classes that know how to find static files in
