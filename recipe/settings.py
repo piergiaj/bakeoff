26dc8b5a0462e568c1f5cbe0a97e439bd728f93c
Erik Sanders <sanderej@rose-hulman.edu>
2013-04-17 02:51:34 -0400

    hel

--- recipe/settings.py
+++ recipe/settings.py
@@ -63,11 +63,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.abspath('staticfiles/')
+STATIC_ROOT = os.path.abspath('staticfiles')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/staticcheese/'
+STATIC_URL = '/staticfiles/'
 #STATIC_URL = 'http://infinite-garden-1600.herokuapp.com/'
 
 #ADMIN_MEDIA_PREFIX = 'http://infinite-garden-1600.herokuapp.com/'
